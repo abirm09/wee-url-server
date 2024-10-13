fe2cf7d4a71c3f872ef470c0829f055a33828c4e
Md. Abir mahmud <abirmahmud5665@gmail.com>
2024-10-13 12:10:48 +0600

    done : setup vercel config

--- src/app.ts
+++ src/app.ts
@@ -3,4 +3,8 @@ import express, { Application } from "express";
 // App Instance
 const app: Application = express();
 
+app.get("/", (req, res) => {
+  res.json({ message: "Server is running" });
+});
+
 export default app;
