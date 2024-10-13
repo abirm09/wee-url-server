0ab5dff32296adf1d266337d9c065bae6ea97aad
Md. Abir mahmud <abirmahmud5665@gmail.com>
2024-10-13 10:22:38 +0600

    done : Setup eslint, prettier, huskey, and lint-staged

---
+++ eslint.config.mjs
@@ -0,0 +1,25 @@
+import globals from "globals";
+import pluginJs from "@eslint/js";
+import tseslint from "typescript-eslint";
+import eslintConfigPrettier from "eslint-config-prettier";
+export default [
+  {
+    ignores: ["dist"],
+  },
+  { files: ["**/*.{js,mjs,cjs,ts}"] },
+  { files: ["**/*.js"], languageOptions: { sourceType: "commonjs" } },
+  { languageOptions: { globals: globals.node } },
+  pluginJs.configs.recommended,
+  ...tseslint.configs.recommended,
+  eslintConfigPrettier,
+  {
+    rules: {
+      "@typescript-eslint/no-unused-vars": "error",
+      "no-console": "error",
+      "no-undef": "error",
+      "no-unused-expressions": "error",
+      "no-unreachable": "error",
+      "@typescript-eslint/consistent-type-definitions": ["error", "type"],
+    },
+  },
+];
